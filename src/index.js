0b91c5e21061becbdcc5c6a93c44cc8a7e8adad5
Dustin Tinney <tinney@gmail.com>
2019-08-20 09:36:23 -0700

    Removes amount adds discount percent

--- src/index.js
+++ src/index.js
@@ -31,15 +31,14 @@ store.dispatch(addInvoice({
   attn: 'Bo <bo@circleci.com>',
   due_date: '10/10/2020',
   notes: 'Send monthly invoices.',
-  amount: 100000,
   projects: [
-    { name: 'Anti-virus Software', description: 'Subscription service for virus software.', price_cents: 20000, quantity: 1},
-    { name: 'AWS', description: 'Monthly server costs.', price_cents: 380000, quantity: 1},
+    { name: 'Anti-virus Software', description: 'Subscription service for virus software.', discount_percent: 40, price_cents: 20000, quantity: 1},
+    { name: 'AWS', description: 'Monthly server costs.', discount_percent: null, price_cents: 380000, quantity: 1},
   ],
   services: [
-    { name: 'Software Development', description: 'Refactor of addresses module.', price_cents: 15000, quantity: 8},
-    { name: 'Software Development', description: 'Exract micro-service.', price_cents: 15000, quantity: 12},
-    { name: 'Software Development', description: 'Deploy new staging environment.', price_cents: 15000, quantity: 9},
+    { name: 'Software Development', description: 'Refactor of addresses module.', discount_percent: null, price_cents: 15000, quantity: 8},
+    { name: 'Software Development', description: 'Exract micro-service.', discount_percent: null, price_cents: 15000, quantity: 12},
+    { name: 'Software Development', description: 'Deploy new staging environment.', discount_percent: null, price_cents: 15000, quantity: 9},
   ],
 }));
 
@@ -49,14 +48,13 @@ store.dispatch(addInvoice({
   attn: 'Matan <Matan@new-relic.com>',
   due_date: '10/20/2020',
   notes: 'Send weekly invoices.',
-  amount: 100000,
   projects: [
-    { name: 'Video Software', description: 'Subscription service for video software.', price_cents: 14000, quantity: 1},
-    { name: 'Heroku', description: 'Monthly server costs.', price_cents: 28000, quantity: 1},
+    { name: 'Video Software', description: 'Subscription service for video software.', discount_percent: null, price_cents: 14000, quantity: 1},
+    { name: 'Heroku', description: 'Monthly server costs.', discount_percent: 50, price_cents: 28000, quantity: 1},
   ],
   services: [
-    { name: 'Software Development', description: 'Refactor of node modules.', price_cents: 15000, quantity: 8},
-    { name: 'Software Development', description: 'Exract service layer.', price_cents: 15000, quantity: 12},
-    { name: 'Software Development', description: 'Deploy product environments.', price_cents: 15000, quantity: 9},
+    { name: 'Software Development', description: 'Refactor of node modules.', discount_percent: null, price_cents: 15000, quantity: 8},
+    { name: 'Software Development', description: 'Exract service layer.', discount_percent: null, price_cents: 15000, quantity: 12},
+    { name: 'Software Development', description: 'Deploy product environments.', discount_percent: null, price_cents: 15000, quantity: 9},
   ],
 }));
